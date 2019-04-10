3c6ab48981e80654e3582f53019b462ce3aa1f71
Matthew Kling <mattkling@gmail.com>
2019-04-10 15:42:55 -0700

    some exploratory analyses

--- analysis.R
+++ analysis.R
@@ -1,4 +1,9 @@
 library(tidyverse)
+library(lme4)
+library(rgdal)
+library(raster)
+select <- dplyr::select
+
 
 # load population and individual tables
 p <- read_csv("pop_info_9-Feb-2019.csv") %>%
@@ -19,6 +24,7 @@ i <- read_tsv("Finalrevised2analysis_Qd2018.txt") %>%
 ggplot(i, aes(lwp_MPa, embolism, color=individual, order=lwp_MPa)) +
       geom_line() +
       facet_grid(popnumber ~ site + tissue)
+ggsave(filename = paste0("figures/1.png"), width=8, height=8, units="in")
 
 # investigate the weirdo
 i %>%
@@ -30,6 +36,7 @@ i %>%
       ggplot(aes(lwp_MPa, embolism, color=individual, order=lwp_MPa)) +
       geom_point() +
       facet_grid(popnumber ~ site + tissue)
+ggsave(filename = paste0("figures/2.png"), width=8, height=8, units="in")
 # seems to have two curves. huh.
 
 # isolate P50, ghetto-style
@@ -45,13 +52,16 @@ p50 <- i %>%
       ungroup() %>%
       rename(p50 = lwp_MPa) %>%
       left_join(p) %>%
-      mutate(popnumber=factor(popnumber))
+      mutate(popnumber=factor(popnumber),
+             individual=paste(popnumber, individual))
 
 # some exploratory plots
 p %>%
       ggplot(aes(longitude, latitude, color=cwd)) +
       geom_point() +
       scale_color_gradientn(colours=c("blue", "green", "yellow", "red"))
+ggsave(filename = paste0("figures/3.png"), width=8, height=8, units="in")
+
 
 p50 %>%
       select(-embolism) %>%
@@ -62,6 +72,8 @@ p50 %>%
       geom_line() +
       geom_point() +
       coord_fixed()
+ggsave(filename = paste0("figures/4.png"), width=8, height=8, units="in")
+
 
 p50 %>%
       select(-embolism) %>%
@@ -74,19 +86,24 @@ p50 %>%
       geom_point(aes(shape=tissue)) +
       coord_fixed() +
       scale_color_viridis_c()
+ggsave(filename = paste0("figures/5.png"), width=8, height=8, units="in")
 
 
 p50 %>%
       group_by(popnumber, site, tissue) %>%
       summarize(p50 = mean(p50), cwd=mean(cwd)) %>%
-      ggplot(aes(site, p50, group=popnumber, color=cwd)) +
+      ggplot(aes(site, p50, group=popnumber, color=popnumber)) +
       geom_line() +
       facet_grid(.~tissue)
+ggsave(filename = paste0("figures/6.png"), width=8, height=8, units="in")
+
 
 p50 %>%
       ggplot(aes(tissue, p50, color=site)) +
       geom_point() +
       facet_grid(.~popnumber)
+ggsave(filename = paste0("figures/7.png"), width=8, height=8, units="in")
+
 
 p50 %>%
       group_by(popnumber, site, tissue) %>%
@@ -98,6 +115,8 @@ p50 %>%
       geom_line() +
       geom_errorbar(alpha=.5) +
       geom_point()
+ggsave(filename = paste0("figures/8.png"), width=8, height=8, units="in")
+
 
 p50 %>%
       group_by(popnumber, site, tissue) %>%
@@ -107,6 +126,9 @@ p50 %>%
       geom_point(size=5) +
       facet_grid(tissue ~ site) +
       scale_color_gradientn(colours=c("magenta", "gray20", "green"))
+ggsave(filename = paste0("figures/9.png"), width=8, height=8, units="in")
+
+
 
 p50 %>%
       group_by(popnumber, site, tissue) %>%
@@ -117,17 +139,36 @@ p50 %>%
       geom_point(size=5) +
       facet_grid(tissue ~ site) +
       scale_color_gradientn(colours=c("magenta", "gray20", "green"))
+ggsave(filename = paste0("figures/10.png"), width=8, height=8, units="in")
 
 
 # we'd expect stronger djf response in stems than leaves
 # i.e. interaction between tissue and temperature
+p50 %>%
+      group_by(popnumber, site, tissue) %>%
+      #summarize(p50 = mean(p50), 
+      #          jja=mean(jja), 
+      #          djf=mean(djf)) %>%
+      gather(var, clim, jja, djf) %>%
+      ggplot(aes(clim, p50, color=tissue)) +
+      geom_point() +
+      geom_smooth(method=lm) +
+      facet_grid(site ~ var, scales="free_x")
+
+p50 %>% 
+      lmer(p50 ~ djf * tissue + site + (1 | popnumber/individual), data=.) %>%
+      summary()
+
+
 p50 %>% 
       lmer(p50 ~ djf + jja + tissue + site + (1 | popnumber/individual), data=.) %>%
       summary()
 
-# we'd expect stronger response to tmin in stems
-# and to tmax/cwd in leaves
 
+
+p50 %>% 
+      lm(p50 ~ djf * cwd * tissue, data=.) %>%
+      summary()
 
 
 
@@ -155,7 +196,7 @@ p50 %>%
 # what if we look just at the most (or least) resilient individual per popn?
 p50 %>%
       group_by(popnumber, site, tissue) %>%
-      summarize_at(vars(p50:aet), min) %>%
+      summarize_at(vars(p50:aet), max) %>%
       lm(p50 ~ jja + djf + tissue + site, data=.) %>%
       summary()
 
@@ -165,17 +206,175 @@ pw_p50_diff ~ pw_cwd_diff + geo_dist
 
 ps <- p50 %>% filter(site=="Wild")
 diffs <- data.frame(p50 = outer(ps$p50, ps$p50, '-') %>% as.vector(),
-             cwd = outer(ps$cwd, ps$cwd, '-') %>% as.vector(),
-             djf = outer(ps$djf, ps$djf, '-') %>% as.vector(),
-             dist = dist(select(ps, longitude, latitude)) %>% 
-                   as.matrix() %>% as.vector())
+                    cwd = outer(ps$cwd, ps$cwd, '-') %>% as.vector(),
+                    djf = outer(ps$djf, ps$djf, '-') %>% as.vector(),
+                    dist = dist(select(ps, longitude, latitude)) %>% 
+                          as.matrix() %>% as.vector())
 
 lm(p50 ~ cwd + djf + dist, data=diffs %>% filter(cwd > 0)) %>%
       summary()
 
-ggplot(diffs %>% filter(cwd>0), aes(cwd, p50)) + geom_point() + geom_smooth()
+ggplot(diffs %>% filter(cwd>0), aes(cwd, p50)) + 
+      geom_point() + geom_smooth()
+ggsave(filename = paste0("figures/10.png"), width=8, height=8, units="in")
 
 
 
 
-# smoothing neighborhood, using herbarium popns
+
+#####################
+
+d <- readOGR("E:/edges/range-edges/data/species_ranges/little_trees/Quercus douglasii",
+             "querdoug")
+
+climate <- stack("E:/edges/range-edges/data/species_ranges/climate/climate_data.tif") %>%
+      crop(d)
+names(climate) <- c("AET", "CWD", "PPT", "JJA", "DJF")
+climate$JJA <- climate$JJA/10
+climate$DJF <- climate$DJF/10
+
+clim <- mask(climate, d)
+
+climd <- clim %>%
+      rasterToPoints() %>%
+      as.data.frame()
+
+
+climd %>%
+      select(x, y, CWD, JJA, DJF) %>%
+      gather(var, value, CWD:DJF) %>%
+      group_by(var) %>%
+      mutate(value=scales::rescale(value)) %>%
+      ggplot() +
+      geom_raster(aes(x, y, fill=value)) +
+      geom_point(data=p, aes(longitude, latitude), size=3) +
+      facet_wrap(~var) +
+      theme_void() +
+      scale_fill_gradientn(colours=c("darkgreen", "yellow", "darkred")) +
+      coord_fixed(ratio=1.2) +
+      theme(legend.position="top")
+ggsave(filename = paste0("figures/13.png"), width=8, height=8, units="in")
+
+
+
+
+
+# smoothing neighborhood
+
+library(dismo)
+radii <- c(2, 10, 20, 50, 100, 250)
+circ <- radii %>%
+      lapply(function(x){
+            clm <- clim
+            if(x==2) clm <- climate
+            circles(select(p, longitude, latitude), 
+                    d=x*1000, lonlat=T, dissolve=F) %>%
+                  as("SpatialPolygons") %>%
+                  raster::extract(clm, .) %>%
+                  sapply(function(y) apply(y, 2, mean, na.rm=T)) %>%
+                  t() %>%
+                  as.data.frame() %>%
+                  mutate(popnumber=p$popnumber) %>%
+                  gather(var, value, -popnumber) %>%
+                  mutate(radius=x)
+            }) %>%
+      do.call("rbind", .) %>%
+      left_join(p)
+
+circ %>%
+      select(popnumber, var, value, radius) %>%
+      distinct() %>%
+      spread(var, value) %>%
+      select(popnumber, radius, CWD, JJA, DJF) %>%
+      ecoclim::pairsData(xy_vars=c("CWD", "JJA", "DJF"),
+                         z_vars=c("popnumber", "radius"),
+                         mirror=T) %>%
+      mutate(x_var = factor(x_var, levels=c("CWD", "JJA", "DJF")),
+             y_var = factor(y_var, levels=c("CWD", "JJA", "DJF"))) %>%
+      ggplot(aes(x_value, y_value, color=radius, group=radius)) +
+      geom_point() +
+      geom_smooth(method=lm, se=F) +
+      facet_grid(y_var ~ x_var, scales="free") +
+      scale_color_gradientn(colours=c("black", "blue", "red", "goldenrod1"),
+                            trans="log10")
+ggsave(filename = paste0("figures/14.png"), width=8, height=8, units="in")
+
+
+p50 <- i %>%
+      group_by(individual, popnumber,
+               site, tissue) %>%
+      filter(embolism <= 50) %>%
+      arrange(desc(embolism)) %>%
+      slice(1) %>%
+      ungroup() %>%
+      rename(p50 = lwp_MPa) %>%
+      left_join(circ) %>%
+      mutate(popnumber=factor(popnumber),
+             individual=paste(popnumber, individual))
+
+p50 %>%
+      group_by(popnumber, site, tissue, var, radius) %>%
+      summarize(p50_se = sd(p50)/sqrt(n()),
+                p50 = mean(p50), 
+                value=mean(value)) %>%
+      ggplot(aes(value, p50, ymin=p50-p50_se, ymax=p50+p50_se,
+                 color=tissue, linetype=site, shape=site)) +
+      geom_point() +
+      geom_smooth(method=lm, se=F) +
+      facet_grid(radius~var, scales="free") +
+      labs(x="climate")
+ggsave(filename = paste0("figures/15.png"), width=8, height=8, units="in")
+
+
+
+# super basic LM with JJA and DJF, for stems in the wild
+p50 %>%
+      filter(radius==20, tissue=="stem", site=="Wild") %>%
+      spread(var, value) %>%
+      lm(p50 ~ JJA + DJF, data=.) %>%
+      summary()
+
+# super basic LM with JJA and DJF, across entire dataset
+p50 %>%
+      filter(radius==20) %>%
+      spread(var, value) %>%
+      lm(p50 ~ JJA + DJF, data=.) %>%
+      summary()
+
+# LMM with JJA and DJF
+p50 %>%
+      filter(radius==20) %>%
+      spread(var, value) %>%
+      lmer(p50 ~ DJF + JJA + site + tissue + 
+                 (1|popnumber/individual), data=.) %>%
+      summary()
+
+# testing for interaction between climate and tissue
+p50 %>%
+      filter(radius==20) %>%
+      spread(var, value) %>%
+      lmer(p50 ~ tissue * DJF + tissue * JJA + site + 
+                 (1|popnumber/individual), data=.) %>%
+      summary()
+
+md <- p50 %>%
+      filter(radius==20) %>%
+      spread(var, value) %>%
+      select(popnumber, individual, site, tissue, p50, JJA, DJF)
+m <- md %>% lm(p50 ~ tissue * DJF * JJA, data=.)
+
+pd <- expand.grid(DJF=0:5, JJA=28:35, tissue=c("leaf", "stem")) %>%
+      mutate(pred = predict(m, .))
+
+ggplot() +
+      geom_contour(data=pd, 
+                   aes(DJF, JJA, z=pred, linetype=tissue, color=..level..),
+                   size=1) +
+      geom_point(data=md, aes(DJF, JJA), color="black", size=3) +
+      scale_color_gradientn(colors=c("yellow", "orange", "red", "purple", "blue", "darkblue"))
+
+ggplot(pd, aes(DJF, pred, group=paste(JJA, tissue), color=tissue)) +
+      geom_line()
+ggplot(pd, aes(JJA, pred, group=paste(DJF, tissue), color=tissue)) +
+      geom_line()
+
